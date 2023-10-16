210a14a63d1e893c27ad8f80446722ffa6cae2ae
snayderPinto88-58 <54647990+snayderPinto88-58@users.noreply.github.com>
2023-10-16 10:29:23 +0200

    agregando Models Materia

---
+++ UniversidadApi/UniversidadApi/Models/Materia.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace UniversidadApi.Models
+{
+    public class Materiacs
+    {
+        [Key]
+        public int Id_materia { get; set; }
+        public string nombreMateria { get; set; }
+
+        [ForeignKey("Docentes")]
+        public int Id_docente { get; set; }
+        public Docentes Docentes { get; set; }
+    }
+}
